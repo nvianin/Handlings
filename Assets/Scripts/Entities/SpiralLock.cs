a5765f8c6c78c7f1a627ac91dfae924f6904d6aa
nvianin <31324783+nvianin@users.noreply.github.com>
2022-01-10 15:44:52 +0100

    update 21 files

---
+++ Assets/Scripts/Entities/SpiralLock.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpiralLock : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
