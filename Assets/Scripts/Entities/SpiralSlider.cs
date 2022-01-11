af6cd83dbfeec0657fa325a7f3cb8a8e44ad4cc6
nvianin <31324783+nvianin@users.noreply.github.com>
2022-01-11 13:03:07 +0100

    hj

--- Assets/Scripts/Entities/SpiralSlider.cs
+++ Assets/Scripts/Entities/SpiralSlider.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class SpiralSlider : MonoBehaviour
 {
     public GameObject spiralRail;
-    private Mesh spiralMesh;
+    Mesh spiralMesh;
+    Vector3[] vertices;
     void Start()
     {
         spiralMesh = spiralRail.GetComponent<MeshFilter>().mesh;
+        vertices = spiralMesh.vertices;
 
-        foreach (Vector3 vert in spiralMesh.vertices)
+        foreach (Vector3 vert in vertices)
         {
             print(vert);
         }
@@ -23,6 +25,9 @@ public class SpiralSlider : MonoBehaviour
     Vector3 GetPositionAt(float t)
     {
         t = Mathf.Clamp01(t);
+        float t_tenth = Mathf.Floor(t*10)/10;
+
+
         return Vector3.zero;
     }
 }
