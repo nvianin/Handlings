b3a066e3745dc0de7583a91acbc876a5445285ce
nvianin <31324783+nvianin@users.noreply.github.com>
2022-01-15 16:44:27 +0100

    sdfg

---
+++ Assets/Scripts/Entities/NotifButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class NotifButton : MonoBehaviour
+{
+    public string type = "snap";
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Entities/NotifHand.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NotifHand : MonoBehaviour
+{
+    public GameObject snap_button;
+    public GameObject whats_button;
+    public GameObject mess_button;
+    void Start()
+    {
+
+
+    }
+    void Update()
+    {
+
+    }
+}
