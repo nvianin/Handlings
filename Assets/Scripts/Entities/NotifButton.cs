fb85a41b9d9c6356aa919700fe26b6b11d25316f
nvianin <31324783+nvianin@users.noreply.github.com>
2022-01-18 09:30:22 +0100

    update 31 files and delete 2 files

--- Assets/Scripts/Entities/NotifButton.cs
+++ Assets/Scripts/Entities/NotifButton.cs
@@ -3,48 +3,68 @@ using System.Collections;
 using System.Collections.Generic;
 public class NotifButton : MonoBehaviour
 {
-    AudioSource audioSource;
+    AudioSource vibration;
+    AudioSource notification;
     GameObject[] buttons = new GameObject[3];
     public GameObject button;
     BoxCollider collider;
-    float pressed = 0;
+    CollisionListener collisionListener;
+    public float pressed = 0;
+    float prevPressed = 0;
+    public bool playerColliding = false;
+    public float TravelDistance = .1f;
+    public bool CanPlay = true;
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        AudioSource[] components = new AudioSource[2];
+        components = GetComponents<AudioSource>();
+        vibration = components[0];
+        notification = components[1];
+
         for (int i = 0; i < 3; i++)
         {
             buttons[i] = transform.GetChild(i + 1).gameObject;
             if (buttons[i].activeInHierarchy) button = buttons[i];
         }
         collider = button.GetComponent<BoxCollider>();
+        collisionListener = button.GetComponent<CollisionListener>();
+        collisionListener.onEnter = (Collision collision) =>
+        {
+            playerColliding = true;
+            print("FUCK");
+        };
+        collisionListener.onExit = (Collision collision) =>
+        {
+            playerColliding = false;
+        };
     }
     void Update()
     {
-
-        Vector3 newPos = button.transform.position;
-
-        newPos.y = -pressed;
-
-        button.transform.position = newPos;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        if (playerColliding && pressed < 1)
         {
-            print("player colliding");
-            RaycastHit hit;
-            if (Physics.Raycast(collision.gameObject.transform.position, Vector3.down, out hit, 1f))
+            pressed = Mathf.Lerp(pressed, 1, Time.deltaTime * 15f);
+            if (CanPlay && pressed >= .8f)
             {
-                if (hit.rigidbody.gameObject.tag == "Button")
-                {
-                    print("player on button");
-                }
+                vibration.Play();
+                notification.Play();
+                CanPlay = false;
             }
         }
-    }
-    void OnCollisionExit(Collision other)
-    {
+        else if (pressed > 0)
+        {
+            pressed = Mathf.Lerp(pressed, 0, Time.deltaTime * 15f);
+            if (pressed < .2f) { CanPlay = true; }
+        }
 
+        if (prevPressed != pressed)
+        {
+            Vector3 newPos = button.transform.position;
+
+            newPos.y = -pressed * TravelDistance;
+
+            button.transform.position = newPos;
+        }
+
+        prevPressed = pressed;
     }
 }
--- Assets/Scripts/Utils/CollisionListener.cs
+++ Assets/Scripts/Utils/CollisionListener.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
+using System;
 
 public class CollisionListener : MonoBehaviour
 {
-    public void onEnter
-    {
-
-    }
-    public void onExit
-    {
-
-    }
+    public Action<Collision> onEnter;
+    public Action<Collision> onExit;
     void OnCollisionEnter(Collision collision)
     {
+        if (onEnter != null) onEnter(collision);
         print("collision enter");
     }
     void OnCollisionExit(Collision other)
     {
+        if (onExit != null) onExit(other);
         print("collision exit");
     }
 }
